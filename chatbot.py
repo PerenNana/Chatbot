072527a209d0dfddcb16207e0ffbc9a9298bca5a
PerenNana <paul.nana-nana@eoniq.ai>
2025-09-20 14:59:44 +0200

    Remove .env file with secrets

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ import tools
 load_dotenv()
 
 
-openai_api_key = os.getenv("...")
+openai_api_key = os.getenv("OPENAI_API_KEY")
 # Initialize the chat model
 llm = init_chat_model("openai:gpt-4.1")
 
